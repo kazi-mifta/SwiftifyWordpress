f332fc0e727632c94499c956d29580f4ad6b06cb
Kazi Miftahul Hoque <kazimifta13@gmail.com>
2021-06-12 22:39:11 +0600

    Added Unit Test
    Added Unit Testing For JSON Decoding and Async Network Call

--- Tests/iOSPressTests/iOSPressTests.swift
+++ Tests/iOSPressTests/iOSPressTests.swift
@@ -8,7 +8,53 @@ final class iOSPressTests: XCTestCase {
         // results.
         //XCTAssertEqual(iOSPress().text, "Hello, World!")
     }
-
+    
+    func testJSONMapping() throws {
+        
+        let json = """
+        {
+            "id":37784,
+            "title":{
+                "rendered":"Top 15 Stunning Examples of the WordPress Elementor Theme in Action"
+            },
+            "date":"2021-06-07T08:23:18"
+        }
+        """
+        guard let jsonData = json.data(using: .utf8) else{
+            XCTFail("Failed to read Data from JSON")
+            return
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .formatted(DateFormatter.standardT)
+        
+        guard let post = try? decoder.decode(Post.self, from: jsonData) else {
+            XCTFail("Failed to decode from data")
+            return
+        }
+        
+        XCTAssertEqual(post.id,37784)
+        XCTAssertEqual(post.title?.rendered,"Top 15 Stunning Examples of the WordPress Elementor Theme in Action")
+        XCTAssertEqual(post.link, nil)
+        
+    }
+    
+    func testAsyncNetworkCall() {
+        let expectation = XCTestExpectation(description: "We should wait 10 sec")
+        
+        NetworkManager.request(baseUrl: "winningwp.com") { [weak expectation]
+            (result: Result<[Post], Error>) in
+            switch result {
+            case .success(let response):
+                XCTAssertEqual(response.count, 20,"For now the request returns 20 post")
+                expectation?.fulfill()
+            case .failure(_):
+                break
+            }
+        }
+        self.wait(for: [expectation], timeout: 10)
+    }
+    
     static var allTests = [
         ("testExample", testExample),
     ]
